aeb9e1b3dc9089bf2afe13d56498228f1268efcc
trndc91 <tritridac@gmail.com>
2025-12-03 15:52:24 +0100

    Adjust resource thresholds and implement alert function
    Lower resource usage thresholds and add alert function.

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -3,6 +3,9 @@ df -m -h
 free -m -h
 
 #!/bin/bash
-CPU_THRESHOLD=80
-MEMORY_THRESHOLD=80
-DISK_THRESHOLD=80
+CPU_THRESHOLD=10
+MEMORY_THRESHOLD=10
+DISK_THRESHOLD=10
+send_alert() {
+  echo "$(tput setaf 1)ALERT: $1 usage exceeded threshold! Current value: $2%$(tput sgr0)"
+}
