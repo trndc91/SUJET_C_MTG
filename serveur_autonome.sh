fd7409a0f57b82d0b66954e6c7a8f0a1c72a72f2
trndc91 <tritridac@gmail.com>
2025-12-03 15:59:27 +0100

    Update serveur_autonome.sh

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -3,10 +3,10 @@ df -m -h
 free -m -h
 
 #!/bin/bash
-CPU_THRESHOLD=10
-MEMORY_THRESHOLD=10
-DISK_THRESHOLD=10
+CPU_THRESHOLD=80
+MEMORY_THRESHOLD=90
+DISK_THRESHOLD=85
 send_alert() {
   echo "$(tput setaf 1)ALERT: $1 usage exceeded threshold! Current value: $2%$(tput sgr0)"
 }
-send_alert "CPU" 10
+send_alert "WARNING" 85
