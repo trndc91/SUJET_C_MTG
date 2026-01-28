a6cab4e941f5a7775f99be32ad85aed0ab5310aa
azaflo <Florian.orchiller@gmail.com>
2026-01-28 11:05:07 +0100

    Refactor serveur_autonome.sh for clarity
    Removed unnecessary commands and adjusted alert formatting.

--- serveur_autonome.sh
+++ serveur_autonome.sh
@@ -1,12 +1,9 @@
-#bin!/bash/
-df -m -h  
-free -m -h
-
 #!/bin/bash
+
 CPU_THRESHOLD=80
 MEMORY_THRESHOLD=90
 DISK_THRESHOLD=85
+
 send_alert() {
-  echo "$(tput setaf 1)ALERT: $1 usage exceeded threshold! Current value: $2%$(tput sgr0)"
+  echo "ALERT: $1 usage exceeded threshold! Current value: $2%"
 }
-send_alert "WARNING" 85
